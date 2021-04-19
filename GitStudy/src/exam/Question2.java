859ee5b8da77fd755798d46ad09c34b425af1f6a
Hyunsik Kim <kosin3@naver.com>
2021-04-19 14:46:08 +0900

    branch b 커밋+1 주석추가

--- GitStudy/src/exam/Question2.java
+++ GitStudy/src/exam/Question2.java
@@ -7,6 +7,9 @@ public class Question2 {
 		System.out.println(Math.random());
 		
 		System.out.println("Random!!");
+		
+		//branchA,B 모두에 커밋을 2개 추가하고 history 내역을 보낼 것
+		//(단, Question2만 수정해야함 ※이 주석은 branchB에서 작성되었음
 	}
 
 }
